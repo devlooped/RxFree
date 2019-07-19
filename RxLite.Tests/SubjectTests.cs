b03874a8177f5ce0190d178c3bbc9c02c14e3c53
Daniel Cazzulino <daniel@cazzulino.com>
2019-07-19 16:20:15 -0300

    Initial port of simple Rx lite for lib authors

---
+++ RxLite.Tests/SubjectTests.cs
@@ -0,0 +1,223 @@
+﻿using Xunit;
+
+namespace System
+{
+    public class SubjectTests
+    {
+        [Fact]
+        public void OnNext()
+        {
+            var subject = new Subject<int>();
+            var value = 0;
+
+            var subscription = subject.Subscribe(i => value += i);
+
+            subject.OnNext(1);
+            Assert.Equal(1, value);
+
+            subject.OnNext(1);
+            Assert.Equal(2, value);
+
+            subscription.Dispose();
+
+            subject.OnNext(1);
+            Assert.Equal(2, value);
+        }
+
+        [Fact]
+        public void OnNextDisposed()
+        {
+            var subject = new Subject<int>();
+
+            subject.Dispose();
+
+            Assert.Throws<ObjectDisposedException>(() => subject.OnNext(1));
+        }
+
+        [Fact]
+        public void OnNextDisposedSubscriber()
+        {
+            var subject = new Subject<int>();
+            var value = 0;
+
+            subject.Subscribe(i => value += i).Dispose();
+
+            subject.OnNext(1);
+
+            Assert.Equal(0, value);
+        }
+
+        [Fact]
+        public void OnCompleted()
+        {
+            var subject = new Subject<int>();
+            var completed = false;
+
+            var subscription = subject.Subscribe(_ => { }, () => completed = true);
+
+            subject.OnCompleted();
+
+            Assert.True(completed);
+        }
+
+        [Fact]
+        public void OnCompletedNoOp()
+        {
+            var subject = new Subject<int>();
+
+            var subscription = subject.Subscribe(_ => { });
+
+            subject.OnCompleted();
+        }
+
+        [Fact]
+        public void OnCompletedOnce()
+        {
+            var subject = new Subject<int>();
+            var completed = 0;
+
+            var subscription = subject.Subscribe(_ => { }, () => completed++);
+
+            subject.OnCompleted();
+
+            Assert.Equal(1, completed);
+
+            subject.OnCompleted();
+
+            Assert.Equal(1, completed);
+        }
+
+        [Fact]
+        public void OnCompletedDisposed()
+        {
+            var subject = new Subject<int>();
+
+            subject.Dispose();
+
+            Assert.Throws<ObjectDisposedException>(() => subject.OnCompleted());
+        }
+
+        [Fact]
+        public void OnCompletedDisposedSubscriber()
+        {
+            var subject = new Subject<int>();
+            var completed = false;
+
+            subject.Subscribe(_ => { }, () => completed = true).Dispose();
+
+            subject.OnCompleted();
+
+            Assert.False(completed);
+        }
+
+        [Fact]
+        public void OnError()
+        {
+            var subject = new Subject<int>();
+            var error = false;
+
+            var subscription = subject.Subscribe(_ => { }, e => error = true);
+
+            subject.OnError(new Exception());
+
+            Assert.True(error);
+        }
+
+        [Fact]
+        public void OnErrorOnce()
+        {
+            var subject = new Subject<int>();
+            var errors = 0;
+
+            var subscription = subject.Subscribe(_ => { }, e => errors++);
+
+            subject.OnError(new Exception());
+
+            Assert.Equal(1, errors);
+
+            subject.OnError(new Exception());
+
+            Assert.Equal(1, errors);
+        }
+
+        [Fact]
+        public void OnErrorDisposed()
+        {
+            var subject = new Subject<int>();
+
+            subject.Dispose();
+
+            Assert.Throws<ObjectDisposedException>(() => subject.OnError(new Exception()));
+        }
+
+        [Fact]
+        public void OnErrorDisposedSubscriber()
+        {
+            var subject = new Subject<int>();
+            var error = false;
+
+            subject.Subscribe(_ => { }, e => error = true).Dispose();
+
+            subject.OnError(new Exception());
+
+            Assert.False(error);
+        }
+
+        [Fact]
+        public void OnErrorRethrowsByDefault()
+        {
+            var subject = new Subject<int>();
+
+            var subs = subject.Subscribe(_ => { });
+
+            Assert.Throws<ArgumentException>(() => subject.OnError(new ArgumentException()));
+        }
+
+        [Fact]
+        public void OnErrorNullThrows()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Subject<int>().OnError(null));
+        }
+
+
+        [Fact]
+        public void SubscribeNullThrows()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Subject<int>().Subscribe(null));
+        }
+
+        [Fact]
+        public void SubscribeDisposedThrows()
+        {
+            var subject = new Subject<int>();
+
+            subject.Dispose();
+
+            Assert.Throws<ObjectDisposedException>(() => subject.Subscribe(_ => { }));
+        }
+
+        [Fact]
+        public void SubscribeOnCompleted()
+        {
+            var subject = new Subject<int>();
+            subject.OnCompleted();
+            var completed = false;
+
+            subject.Subscribe(_ => { }, () => completed = true).Dispose();
+
+            Assert.True(completed);
+        }
+
+        [Fact]
+        public void SubscribeOnError()
+        {
+            var subject = new Subject<int>();
+            subject.OnError(new Exception());
+            var error = false;
+
+            subject.Subscribe(_ => { }, e => error = true);
+
+            Assert.True(error);
+        }
+    }
+}
---
+++ RxLite/System/ObservableExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace System
+{
+    /// <summary>
+    /// Provides a set of static methods for subscribing delegates to observables.
+    /// </summary>
+    internal static class ObservableExtensions
+    {
+        static readonly Action<Exception> rethrow = e => ExceptionDispatchInfo.Capture(e).Throw();
+        static readonly Action nop = () => { };
+
+        /// <summary>
+        /// Subscribes to the observable providing just the <paramref name="onNext"/> delegate.
+        /// </summary>
+        public static IDisposable Subscribe<T>(this IObservable<T> source, Action<T> onNext)
+            => Subscribe(source, onNext, rethrow, nop);
+
+        /// <summary>
+        /// Subscribes to the observable providing both the <paramref name="onNext"/> and 
+        /// <paramref name="onError"/> delegates.
+        /// </summary>
+        public static IDisposable Subscribe<T>(this IObservable<T> source, Action<T> onNext, Action<Exception> onError)
+            => Subscribe(source, onNext, onError, nop);
+
+        /// <summary>
+        /// Subscribes to the observable providing both the <paramref name="onNext"/> and 
+        /// <paramref name="onCompleted"/> delegates.
+        /// </summary>
+        public static IDisposable Subscribe<T>(this IObservable<T> source, Action<T> onNext, Action onCompleted)
+            => Subscribe(source, onNext, rethrow, onCompleted);
+
+        /// <summary>
+        /// Subscribes to the observable providing all three <paramref name="onNext"/>, 
+        /// <paramref name="onError"/> and <paramref name="onCompleted"/> delegates.
+        /// </summary>
+        public static IDisposable Subscribe<T>(this IObservable<T> source, Action<T> onNext, Action<Exception> onError, Action onCompleted)
+            => source.Subscribe(new AnonymousObserver<T>(onNext, onError, onCompleted));
+
+        class AnonymousObserver<T> : IObserver<T>
+        {
+            readonly Action<T> onNext;
+            readonly Action<Exception> onError;
+            readonly Action onCompleted;
+
+            public AnonymousObserver(Action<T> onNext, Action<Exception> onError, Action onCompleted)
+            {
+                this.onNext = onNext;
+                this.onError = onError;
+                this.onCompleted = onCompleted;
+            }
+
+            public void OnCompleted() => onCompleted();
+
+            public void OnError(Exception error) => onError(error);
+
+            public void OnNext(T value) => onNext(value);
+        }
+    }}
