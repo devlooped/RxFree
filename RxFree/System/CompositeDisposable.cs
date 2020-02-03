9632d6c60e4ccfce2230f235360975a711a25b3d
Daniel Cazzulino <daniel@cazzulino.com>
2020-02-03 16:44:25 -0300

    Adds CompositeDisposable, mutable and immutable
    The factory method `CompositeDisposable.Create` results
    in an array-based composite that's immutable and also
    more performant. The regular constructor returns a mutable
    one where you can add additional disposables after construction.
    
    An Rx-compatible facade is also provided so code that
    depends on `StableCompositeDisposable.Create` can still
    compile (mostly) as-is.

---
+++ RxFree/System/CompositeDisposable.cs
@@ -0,0 +1,89 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace System
+{
+    /// <summary>
+    /// Represents a group of disposable resources that are disposed together.
+    /// </summary>
+    [GeneratedCode("RxFree", "*")]
+    [CompilerGenerated]
+    internal class CompositeDisposable : IDisposable
+    {
+        bool disposed;
+        readonly ConcurrentBag<IDisposable> disposables;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompositeDisposable"/> class from a group of disposables.
+        /// </summary>
+        /// <param name="disposables">Disposables that will be disposed together.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="disposables"/> is <see langword="null"/>.</exception>
+        public CompositeDisposable(params IDisposable[] disposables)
+        {
+            this.disposables = new ConcurrentBag<IDisposable>(disposables) ?? throw new ArgumentNullException(nameof(disposables));
+        }
+
+        /// <summary>
+        /// Gets a value that indicates whether the object is disposed.
+        /// </summary>
+        public bool IsDisposed => disposed;
+
+        /// <summary>
+        /// Adds a disposable to the <see cref="CompositeDisposable"/> or disposes the disposable if the <see cref="CompositeDisposable"/> is disposed.
+        /// </summary>
+        /// <param name="item">Disposable to add.</param>
+        public void Add(IDisposable disposable)
+        {
+            if (disposed)
+                disposable?.Dispose();
+            else
+                disposables.Add(disposable);
+        }
+
+        /// <summary>
+        /// Disposes all disposables in the group and removes them from the group.
+        /// </summary>
+        public void Dispose()
+        {
+            if (disposed)
+                return;
+
+            disposed = true;
+            while (disposables.TryTake(out var disposable))
+            {
+                disposable?.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Creates a new group of disposable resources that are disposed together.
+        /// </summary>
+        /// <param name="disposables">Disposable resources to add to the group.</param>
+        /// <returns>Group of disposable resources that are disposed together.</returns>
+        public static IDisposable Create(params IDisposable[] disposables) => new DisposableArray(disposables);
+
+        sealed class DisposableArray : IDisposable
+        {
+            IDisposable[] disposables;
+
+            public DisposableArray(IDisposable[] disposables)
+            {
+                Volatile.Write(ref this.disposables, disposables ?? throw new ArgumentNullException(nameof(disposables)));
+            }
+
+            public void Dispose()
+            {
+                var old = Interlocked.Exchange(ref disposables, null);
+                if (old != null)
+                {
+                    foreach (var disposable in old)
+                    {
+                        disposable?.Dispose();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RxFree/System/StableCompositeDisposable.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace System
+{
+    /// <summary>
+    /// For compatibility with Rx
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal class StableCompositeDisposable
+    {
+        /// <summary>
+        /// For compatibility with Rx
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static IDisposable Create(params IDisposable[] disposables) => CompositeDisposable.Create(disposables);
+    }
+}
